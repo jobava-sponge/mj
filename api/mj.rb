dccbc3cc4d492d49447fa32930614aa91429805a
georgiana-b <bere.georgi@gmail.com>
2015-02-21 19:17:50 +0200

    Return XML

--- api/mj.rb
+++ api/mj.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader' if development?
 require './helpers/database'
 require 'savon'
+require 'nori'
 
 module API
   class Mj < Sinatra::Application
@@ -9,11 +10,12 @@ module API
     
     namespace '/api/v1' do
       get '/' do
-        content_type :json
+        content_type  'text/xml' ,  "charset" => "utf-8"
+        # content_type  json ,  "charset" => "utf-8"
         client = Savon.client(endpoint: "http://portalquery.just.ro/Query.asmx", wsdl: "http://portalquery.just.ro/Query.asmx?WSDL", pretty_print_xml: true)
         message = {institutie: "TribunalulTIMIS"}
-        response = client.call(:cautare_dosare, message: message)
-        response.body.to_json
+        response = client.call(:cautare_dosare,message: message)
+        response.to_xml
       end
     end
 

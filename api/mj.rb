a7a0a9fb883bd2ca6348cb43f3a672c747bb471f
Victor Nițu <victor@thesponge.eu>
2015-02-22 15:58:19 +0200

    Fixed the count, some refactoring,  added mongoid

--- api/mj.rb
+++ api/mj.rb
@@ -4,6 +4,7 @@ require './helpers/database'
 require 'savon'
 require 'nori'
 require './helpers/soap'
+require 'pp'
 
 module API
   class Mj < Sinatra::Application
@@ -12,8 +13,15 @@ module API
     namespace '/api/v1' do
       get '/filldb/:institution' do
         coll = settings.db.collection('institutionsFiles')
-        obj = Helpers::Soap.get_inst(params[:institution])
-        coll.insert(obj)
+        obj = Helpers::Soap.new().get_inst(params[:institution])
+        
+        obj.each do |key,val|
+            val = val.to_json
+            val = val.hash
+            coll.insert(val)
+        end
+        
+        #coll.insert(obj)
         status 201
       end
     end
--- helpers/soap.rb
+++ helpers/soap.rb
@@ -3,23 +3,25 @@ require 'nori'
 
 module Helpers
 	class Soap
-		client = Savon.client(endpoint: "http://portalquery.just.ro/Query.asmx", wsdl: "http://portalquery.just.ro/Query.asmx?WSDL", pretty_print_xml: true)
         def get_inst(institutie)
+            client = Savon.client(endpoint: "http://portalquery.just.ro/Query.asmx", wsdl: "http://portalquery.just.ro/Query.asmx?WSDL", pretty_print_xml: true)
         	dstop = Time.now
         	message = {institutie: "#{institutie}", dataStop:dstop.strftime("%Y-%m-%d") }
         
         	response = client.call(:cautare_dosare,message: message)
         	res = response.hash
-        	num_dosare = res[:envelope][:body][:cautare_dosare_response][:cautare_dosare_result].length    
+        	num_dosare = res[:envelope][:body][:cautare_dosare_response][:cautare_dosare_result][:dosar].length    
 
        		if num_dosare > 999
          		while num_dosare > 999
             		dstart = dstop - (60*60*24*10)
             		message = {institutie:"#{institutie}",dataStart:dstart.strftime("%Y-%m-%d"), dataStop:dstop.strftime("%Y-%m-%d") }
-            		num_dosare = client.call(:cautare_dosare,message: message).hash[:envelope][:body][:cautare_dosare_response][:cautare_dosare_result].length 
+                    res = client.call(:cautare_dosare,message: message).hash
+            		num_dosare = res[:envelope][:body][:cautare_dosare_response][:cautare_dosare_result][:dosar].length 
           		end
           		dstop = dstart
         	end
+            return res[:envelope][:body][:cautare_dosare_response][:cautare_dosare_result][:dosar]
         end
 	end
 end
